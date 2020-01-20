64512a43fd81b25ee0df870cd0f897e0ffd36291
Meta <l.szabatura@gmail.com>
2020-01-20 20:02:07 +0100

    Add server register/login

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI: 'mongodb://Meta:test11@ds155916.mlab.com:55916/my-boardgameio-app',
+	secretOrKey: 'secret'
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const pino = require('express-pino-logger')()
 
+const passport = require('passport')
+const users = require('./routes/api/users')
+
 const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
 app.use(pino)
+
+const db = require('./config/keys').mongoURI
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB successfully connected'))
+	.catch((err) => console.log(err))
 
 app.get('/api/greeting', (req, res) => {
 	const name = req.query.name || 'World'
@@ -12,4 +28,15 @@ app.get('/api/greeting', (req, res) => {
 	res.send(JSON.stringify({ greeting: `Hello ${name}!` }))
 })
 
-app.listen(3001, () => console.log('Express server is running on localhost:3001'))
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require('./config/passport')(passport)
+
+// Routes
+app.use('/api/users', users)
+
+const port = process.env.PORT || 3001 // process.env.port is Heroku's port if you choose to deploy the app there
+
+app.listen(port, () => console.log('Express server is running on localhost:3001'))
